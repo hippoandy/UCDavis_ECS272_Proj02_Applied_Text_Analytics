7585bab23d4420125e32812e5e4ecca36905ff61
hippoandy <ycaho@ucdavis.edu>
2018-02-17 07:19:16 +0800

    Variables for NLP analysis and other global variables.

---
+++ public/js/variables.js
@@ -0,0 +1,9 @@
+var nlp_config =
+{
+    "adj_blacklist": [
+        'all', 'even', 'other',
+        'true', 'false',
+        'south', 'north', 'east', 'west',
+        'right', 'left', 'down', 'up',
+    ]
+};
