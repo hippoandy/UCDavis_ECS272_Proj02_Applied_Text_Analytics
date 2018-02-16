b7f28feb4b395c9db5ec13053b9089f22e4a31ce
hippoandy <ycaho@ucdavis.edu>
2018-02-17 07:18:29 +0800

    Codes for creating charts.

---
+++ public/js/charts/thermo.js
@@ -0,0 +1,227 @@
+const noop = () => {};
+
+class D3Component
+{
+    constructor({tagName, className})
+    {
+        this.state = {
+            tagName: tagName || "div", 
+            className,
+            key: undefined
+        };
+
+        let InstanceMetadata = () => {};
+        const selected = (node) => { return (node.nodeName ? d3.select(node) : node); }
+
+        InstanceMetadata.createInstance = (node, d, owner) => {
+            node.__instance__ = {
+            owner,
+            destroy: () => owner.exit(selected(node), d),
+            };
+        }
+
+        InstanceMetadata.applyInstance =  (node, cb) => {
+            const instance = node.__instance__;
+            if(cb && instance) {
+                return cb(instance);
+            } else {
+                return instance;
+            }
+        }
+
+        InstanceMetadata.getOwner = (node) => {
+            return InstanceMetadata.applyInstance(node, (instance) => { 
+                return instance.owner; 
+            });
+        }
+
+        InstanceMetadata.destroyInstance = (owner) => {
+            // first, destroy all descendants
+            selected(owner).selectAll('*').each((d, i, arr) => {
+                const node = arr[i];
+                InstanceMetadata.applyInstance(node, (instance) => { 
+                instance.destroy(node); 
+                });
+            });
+            // now destroy the instance
+            InstanceMetadata.applyInstance(owner, (instance) => {     
+                (instance.destroy(owner) || selected(owner)).remove(); 
+            });
+        }
+        D3Component.select = selected;  
+        D3Component.InstanceMetadata = InstanceMetadata;  
+    }
+    
+
+    mountIn(node, data, context)
+    {
+        const {InstanceMetadata, select} = D3Component;
+        const isOwnedByMe = (node) => {
+            const owner = InstanceMetadata.getOwner(node);
+            if(owner) { return (owner === this || owner.name === "component"); }
+        };
+
+        const children = select(node)
+                            .selectAll((d, i, nodes) => {
+                                const node = nodes[i];
+                                return Array.from(node.children).filter(isOwnedByMe);
+                            })
+                            .data(data);
+
+        this.renderChildren(children);
+        return children;
+    }
+
+    renderChildren( children )
+    {
+        const {tagName, className} = this.state;
+        const {InstanceMetadata} = D3Component;
+
+        // on element creation
+        var mounted = children.enter()
+            .append(tagName).attr('class', className);
+
+        mounted.each((d, i, nodes) => {
+            const node = nodes[i];
+            InstanceMetadata.createInstance(node, d, this);
+            this.enter(node, d);
+            this.merge(node, d);
+        });
+
+        // on data change
+        children.each((d, i, nodes) => {
+            const node = nodes[i];
+            this.merge(node, d);
+        });
+
+        // on exit
+        children.exit()
+            .each((d, i, nodes) => {
+            const node = nodes[i];
+            InstanceMetadata.destroyInstance(node);
+            });
+
+        return children;
+    }
+    enter(_) { /* overwrite in inheriting components */ }
+    merge(_) { /* overwrite in inheriting components */  }
+    exit(_) {  /* overwrite in inheriting components */ }
+}
+
+// This component displays the visualization.
+const {select} = d3;
+
+class Thermometer extends D3Component
+{
+    constructor(props)
+    {
+        super(props);
+        const config = { 
+            rectH: 100, 
+            rectW: 50, 
+            strk: 0, 
+            outerRectBg: "#f3f3f3", 
+            tickColor: "#8c8c8c", 
+            tickQty: 20
+        };
+        this.state = Object.assign(this.state, config);
+    }
+    enter(node, d)
+    {
+        const { rectH, rectW, strk, outerRectBg, tickColor, tickQty } = this.state;
+
+        select(node)
+            .attr("width", rectW+strk)
+            .attr("height", rectH+strk);
+
+        var outerRect = select(node).append("rect")
+            .attr("x", 0).attr("y", 0)
+            .attr("height", rectH).attr("width", rectW)
+            .style("fill",   outerRectBg)
+            .style("stroke", outerRectBg)
+            .style("stroke-width", "" + strk + "px");
+
+        var innerRect = select(node).append("rect").attr("class", "innerRect")
+
+        var scale = d3.range(0, tickQty);
+
+        var tickWidth = 0;
+        var wideTickWidth = 10;
+
+        var lines = select(node).selectAll("line.xScale");
+        var lineEnter = lines.data(scale).enter();
+        lineEnter.append("line").attr("class", "xScale")
+            .attr("x1", strk/2)
+            .attr("y1", function(d) { return (rectH/tickQty * d);})
+            .attr("x2", function(d) { return (strk/2) + ((d % 2 === 0) ? wideTickWidth : tickWidth);})
+            .attr("y2", function(d) { return (rectH/tickQty * d);})
+            .style("stroke", tickColor).style("stroke-width", "1px");
+    }
+    merge(node, d)
+    {
+        const {percent, label, color} = d;
+        const { rectH, rectW, strk, outerRectBg, tickColor } = this.state;
+        select(node).selectAll("rect.innerRect").attr("x", strk/2).attr("y", rectH - percent)
+            .attr("height", percent).attr("width", rectW - strk)
+            .style("fill", color);
+    }
+}
+
+class Bullet  extends D3Component
+{
+    enter(node, d)
+    {
+        select(node).append('div')
+            .attr('class', 'bullet')
+            .html(`
+            <div><span class="dot" style="background-color: ${d.color}"></span><span class="label">${d.label}</span></div>
+            <div><span class="count">${d.count}</span></div>
+            `);
+    }
+}
+
+class ThermoWidget extends D3Component
+{
+    getThermometerData(d)
+    {
+        const {totalCount, usedCount, usedPercentageLabel, levels} = d;
+        const percent = d.usedPercentage = (usedCount >= totalCount) ? 100 : (usedCount/totalCount) * 100;
+        const level = levels.filter((d) => { return percent >= d.threshold; }).slice(-1);
+        const color = d.usedColor = level[0].colour;
+        return {percent, color, label: usedPercentageLabel};
+    }
+
+    enter(node, d)
+    {
+        this.refs = {
+            thermo : new Thermometer({tagName:'svg', className: 'thermometer'}),
+            bullet1 : new Bullet({tagName:'div', className: 'bullet'}),
+            bullet2 : new Bullet({tagName:'div', className: 'bullet'})
+        };
+    }
+    merge(node, d)
+    {
+        const {thermo, bullet1, bullet2} = this.refs;
+        var leftPanel =  select(node).append('div').attr('class', 'panel left');
+        thermo.mountIn(leftPanel, [this.getThermometerData(d)]);
+        leftPanel.append('div').attr('class', 'percent-summary')
+            .html(`<div class="count">${parseFloat(d.usedPercentage).toFixed( 1 )}%</div><div class="label">${d.usedPercentageLabel}</div>`);
+        var rightPanel = select(node).append('div').attr('class', 'panel right');
+        bullet1.mountIn(rightPanel, [{color: "#f3f3f3", count: d.totalCount, label: d.totalCountLabel}]);
+        bullet2.mountIn(rightPanel, [{color: d.usedColor, count: d.usedCount, label:d.usedCountLabel}]);
+    }
+}
+
+const thermo_draw = function( config )
+{
+    // set the label and value
+    var d = config.value.map((d) => {
+        return Object.assign({}, config, {usedCount: d.val, usedPercentageLabel: d.label});
+    });
+
+    const div = document.createElement('div');
+    document.getElementById('gauge-container').appendChild(div);
+
+    const wgt  = new ThermoWidget({tagName:'div', className: 'thermo-widget'});
+    wgt.mountIn(d3.select(div), d);
+};
---
+++ public/js/charts/word-cloud.js
@@ -0,0 +1,87 @@
+
+// Word Cloud
+// Ref. https://bl.ocks.org/abrahamdu/e1481e86dd4e9d553cc2d7d359b91e68
+
+var word_cloud = {
+    draw: function( data )
+    {
+        // // var categories = d3.keys(d3.nest().key(function(d) { return d.category; }).map(data));
+        // // var color = d3.scale.ordinal().range(["#66c2a5","#fc8d62","#8da0cb","#e78ac3","#a6d854"]);
+        var color = d3.scaleBand().rangeRound( ["#66c2a5","#fc8d62","#8da0cb","#e78ac3","#a6d854"] )
+        var fontSize = d3.scalePow().exponent(5).domain([0,1]).range([10,80]);
+
+        var margin = {top: 10, right: 10, bottom: 10, left: 10},
+            width = 460 - margin.left - margin.right,
+            height = 350 - margin.top - margin.bottom;
+
+        var svg = d3.select( '#sider-wc-container' ).append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        var color = d3.scaleOrdinal(d3.schemeCategory20);
+        // var categories = d3.keys(d3.nest().key(function(d) { return d.State; }).map(data));
+        // var fontSize = d3.scalePow().exponent(5).domain([0,1]).range([40,80]);
+        
+        var layout = d3.layout.cloud()
+            .size([width, height])
+            .timeInterval(20)
+            .words(data)
+            .rotate(function(d) { return 0; })
+            .fontSize(function(d,i) { return fontSize(Math.random()); })
+            //.fontStyle(function(d,i) { return fontSyle(Math.random()); })
+            .fontWeight(["bold"])
+            .text(function(d) { return d.Team_CN; })
+            .spiral("rectangular") // "archimedean" or "rectangular"
+            .on("end", draw_wc)
+            .start();
+
+        var wordcloud = svg.append("g")
+            .attr('class','wordcloud')
+            .attr("transform", "translate(" + width/2 + "," + height/2 + ")");
+
+        // Define 'div' for tooltips
+        var div = d3.select( "body" )
+            // declare the tooltip div 
+            .append( "div" )
+            .attr( "class", "tooltip" )
+            .style( "opacity", 0 );
+
+        function draw_wc( words )
+        {
+            wordcloud.selectAll( "text" )
+                .data( words )
+                .enter().append( "text" )
+                .attr( 'class', 'word')
+                .style( "font-size", function(d) { return d.size + "px"; } )
+                .style( "font-family", function(d) { return d.font; } )
+                // .style( "fill", function( d ) { return color( d ); } )
+                .style( "fill", function( d ) { return color( d.word ); } )
+                //.style("fill", function(d) { 
+                    //var paringObject = data.filter(function(obj) { return obj.Team_CN === d.text});
+                // return color(paringObject[0].category); 
+                //})
+                .attr( "text-anchor", "middle" )
+                .attr( "transform", function( d ) { return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")"; } )
+                .text( function(d) { return d.word; } )
+                .on( "mouseover", function( d )
+                {
+                    div.transition()
+                        .duration( 100 )
+                        .style( "visibility", "visible" )
+                        .style( "opacity", 0.9 )
+                        .style( "transition", "0.5s" );
+                    div.html(
+                            "<div class=\"text-bold center\">" + d.word + "</div><br>" +
+                            "Amount: " + d.amount
+                        )
+                        .style( "left", (d3.event.pageX - 90) + "px" )
+                        .style( "top", (d3.event.pageY - 28) + "px" );
+                })
+                .on( "mouseout", function() {
+                    return div.style( "visibility", "hidden" );
+                });
+        }
+    }
+};
